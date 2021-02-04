fe232b03b0e60045c9d99eb232b8a9b596667e6b
sp756897 <akshaysp2090@gmail.com>
2021-02-04 19:26:31 +0530

    Vircom V-5

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -62,7 +62,7 @@ export default class Landing extends Component {
         <h6>Phone: Calc(log10 * sin(50*20*1) * 0</h6>
         <h6>Address: Milky Way Galaxy, Solar System, Mars</h6>
         <h6>Email: LogoutRightNow@goaway.com</h6>
-        <h6>Created with Love : Akshay S P</h6>
+        <h6>Created with Love : Akshay S P </h6>
         </h>
       </div>
     </div>
